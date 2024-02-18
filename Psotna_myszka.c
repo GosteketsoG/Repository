14138c55fa58d9a022e008ae2473ee963490dff1
nickpat1pl <patrykzlwsk@gmail.com>
2024-02-18 17:49:54 +0100

    Tricky nouse problem

---
+++ Psotna_myszka.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float a = 0.1;
+    float b = 0.2;
+    float suma = a + b;
+
+    if (suma == 0.3) {
+        printf("Suma jest rowna 0.3\n");
+    } else {
+        printf("Suma nie jest rowna 0.3, suma to: %f\n", suma);
+    }
+
+    return 0;
+}
